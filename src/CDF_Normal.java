7c04c21efcea43d00b2afe3c5285e1a36c1668dd
dl447 <dereklee9ab@aol.com>
2017-03-02 10:33:18 -0500

    Lit

---
+++ src/CDF_Normal.java
@@ -0,0 +1,109 @@
+/**
+ * Created by dl447 on 3/2/17.
+ */
+public class CDF_Normal {
+    /*
+
+Here is a copy of the documentation in the FORTRAN code:
+
+	SUBROUTINE NORMP(Z, P, Q, PDF)
+C
+C	Normal distribution probabilities accurate to 1.e-15.
+C	Z = no. of standard deviations from the mean.
+C	P, Q = probabilities to the left & right of Z.   P + Q = 1.
+C       PDF = the probability density.
+C
+C       Based upon algorithm 5666 for the error function, from:
+C       Hart, J.F. et al, 'Computer Approximations', Wiley 1968
+C
+C       Programmer: Alan Miller
+C
+C	Latest revision - 30 March 1986
+C
+
+*/
+
+    public static double normp(double z) {
+
+        double zabs;
+        double p;
+        double expntl,pdf;
+
+        final double p0 = 220.2068679123761;
+        final double p1 = 221.2135961699311;
+        final double p2 = 112.0792914978709;
+        final double p3 = 33.91286607838300;
+        final double p4 = 6.373962203531650;
+        final double p5 = .7003830644436881;
+        final double p6 = .3526249659989109E-01;
+
+        final double q0 = 440.4137358247522;
+        final double q1 = 793.8265125199484;
+        final double q2 = 637.3336333788311;
+        final double q3 = 296.5642487796737;
+        final double q4 = 86.78073220294608;
+        final double q5 = 16.06417757920695;
+        final double q6 = 1.755667163182642;
+        final double q7 = .8838834764831844E-1;
+
+        final double cutoff = 7.071;
+        final double root2pi = 2.506628274631001;
+
+        zabs = Math.abs(z);
+
+//  |z| > 37
+
+        if (z > 37.0) {
+
+            p = 1.0;
+
+            return p;
+
+        }
+
+        if (z < -37.0) {
+
+            p = 0.0;
+
+            return p;
+
+        }
+
+//  |z| <= 37.
+
+        expntl = Math.exp(-.5*zabs*zabs);
+
+        pdf = expntl/root2pi;
+
+//  |z| < cutoff = 10/sqrt(2).
+
+        if (zabs < cutoff) {
+
+            p = expntl*((((((p6*zabs + p5)*zabs + p4)*zabs + p3)*zabs +
+                    p2)*zabs + p1)*zabs + p0)/(((((((q7*zabs + q6)*zabs +
+                    q5)*zabs + q4)*zabs + q3)*zabs + q2)*zabs + q1)*zabs +
+                    q0);
+
+        } else {
+
+            p = pdf/(zabs + 1.0/(zabs + 2.0/(zabs + 3.0/(zabs + 4.0/
+                    (zabs + 0.65)))));
+
+        }
+
+        if (z < 0.0) {
+
+            return p;
+
+        } else {
+
+            p = 1.0 - p;
+
+            return p;
+
+        }
+
+    }
+
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,75 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public void method(){
-        double t1 = .5;
-        double t2 = .75;
+    public static double p(double t1, double t2){
         double std1 = Math.sqrt(t1*(1-t1)), std2 = Math.sqrt(t2*1-t2);
         double z = (t1-t2) / (Math.sqrt(std1) + Math.sqrt(Math.exp(std2)));
-      //  System.out.println(CDF_Normal.norp(z));
+        return CDF_Normal.normp(z);
     }
 
     public static void main(String[] args) throws IOException {
         // write your code here
         Scanner txt = new Scanner(new File("RegularSeasonCompactResults.txt"));
-        String tx[] = new String[10000];
-        int maxIndex = -1;
+        ArrayList<String> tm = new ArrayList();
+        ArrayList<Double> wl = new ArrayList();
+        Scanner test = new Scanner(new File("teams.txt"));
+        /*for(int i = 0; test.hasNextLine(); i++){
+            String s = test.nextLine();
+            String[] a = s.split("\\s+");
+            System.out.print(a[2] + ":\t");
+            String[] b = a[4].split("-");
+            int g = Integer.parseInt(b[0]) + Integer.parseInt(b[1]);
+            double winloss = Double.parseDouble(b[0]) / (double) g;
+            System.out.println(winloss);
+        }*/
+        while(test.hasNextLine()){
+            String s[] = test.nextLine().split(",");
+            tm.add(s[0]);
+            wl.add(Double.parseDouble(s[1]));
+
+        }
+      //  for(int i = 0; i < tm.size(); i++){
+        //    for(int u = i + 1; u < tm.size(); u++){
+         //       System.out.println(tm.get(i) + "\t" + tm.get(u) + ":\t" + p(wl.get(i), wl.get(u)));
+         //   }
+       // }
+        for(int i = 0; i < tm.size(); i = i + 2) {
+            System.out.println(tm.get(i) + "\t" + tm.get(i + 1) + ":\t" + p(wl.get(i), wl.get(i + 1)));
+
+            System.out.println();
+            if(p(wl.get(i), wl.get(i + 1)) > .5){
+                   System.out.println(tm.get(i) +"\tWins!");
+
+               }
+            else if(p(wl.get(i), wl.get(i + 1)) == .5){
+                System.out.println("'Tis a tie!");
+            }
+            else{
+                System.out.println(tm.get(i + 1) + "\tWins!");
+
+            }
+            System.out.println();
+
+        }
+        /*int maxIndex = -1;
 
 
         while (txt.hasNext()) {
             maxIndex++;
-            tx[maxIndex] = txt.nextLine();
+            tx.add(txt.nextLine();
 
         }
 
         //txt.close();
         for (int i = 0; i <= maxIndex; i++) {
-            System.out.println(tx[i]);
+            System.out.println(tx.get(i));
         }
         System.out.println("Enter team 1");
         Scanner input = new Scanner(System.in);
         String str = txt.next();
-         String one = input.next();
-
+        String one = input.next();
+        */
 
     }
 }
