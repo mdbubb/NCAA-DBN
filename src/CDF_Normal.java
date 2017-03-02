7d68d15bbe0a1652091185d730e64063c6909832
Lee, Derek <Dereklee9ab@aol.com>
2017-03-02 16:56:28 -0500

    Update CDF_Normal.java

--- src/CDF_Normal.java
+++ src/CDF_Normal.java
@@ -27,7 +27,7 @@ C
 	    //returns the probability of a z to the right of what is inputted
         double zabs; //Absolute value of Z.  
         double p; //Probability that gets returned
-        double expntl,pdf; // -.25 * (Z)^4
+        double expntl,pdf; // -.25 * (Z)^4, density function for getting the area under the curve
         final double p0 = 220.2068679123761;
         final double p1 = 221.2135961699311;
         final double p2 = 112.0792914978709;
@@ -35,7 +35,7 @@ C
         final double p4 = 6.373962203531650;
         final double p5 = .7003830644436881;
         final double p6 = .3526249659989109E-01;
-	    //probability coefficients
+	    //probability coefficients, for the right
         final double q0 = 440.4137358247522;
         final double q1 = 793.8265125199484;
         final double q2 = 637.3336333788311;
@@ -44,7 +44,7 @@ C
         final double q5 = 16.06417757920695;
         final double q6 = 1.755667163182642;
         final double q7 = .8838834764831844E-1;
-	    //more probability coefficients
+	    //more probability coefficients, for the left
         final double cutoff = 7.071;
 	    //z score at which a different function must be used
         final double root2pi = 2.506628274631001;
@@ -75,6 +75,7 @@ C
 
         pdf = expntl/root2pi;
 	    //density function, used for finding the area under the curve
+	    
 //  |z| < cutoff = 10/sqrt(2).
 
         if (zabs < cutoff) {
